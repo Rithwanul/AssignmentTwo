dad35bf026f6728b36766f344d5f26db91c2b213
rithwanul <rithwanul@gmail.com>
2023-10-23 06:22:18 +0600

    setting binding

--- app/src/main/java/com/example/assignmenttwo/MainActivity.kt
+++ app/src/main/java/com/example/assignmenttwo/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.assignmenttwo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.assignmenttwo.databinding.ActivityMainBinding
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.name.text = "Dico"
     }
 }
